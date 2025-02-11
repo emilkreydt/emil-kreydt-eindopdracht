c287ee742913e6f252ae2d81b9e12984f2a12407
emilkreydt <r068240@student.thomasmore.be>
2025-02-11 10:07:46 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-center min-h-screen h-full flex flex-col justify-center items-center gap-y-20 w-full">
+      <h1 className="text-4xl font-bold">emil</h1>
+    </div>
+  );
+}
